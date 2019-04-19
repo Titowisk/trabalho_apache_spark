6c62d4596f841b94fb7750efa2c16c94dd4392c8
Vitor Filardi <rabelo51@gmail.com>
2019-04-18 21:51:10 -0300

    Adição de arquivo teste

--- test_apache_spark.py
+++ test_apache_spark.py
@@ -11,11 +11,86 @@ import os
 spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
 
 
-dataframe = spark.read.text("artigos_teste/teste.txt")
-print(dataframe.collect())
+dataframe = spark.read.text("artigos_teste/teste.txt") # retorna um dataframe
+# select = dataframe.select('*').show()
+# split = dataframe.select(split(dataframe.value, '\s+')).show()
+"""
+Split
++--------------------+
+|   split(value, \s+)|
++--------------------+
+|[Lorem, ipsum, do...|
+|                  []|
+|[Nulla, posuere, ...|
+|                  []|
+|[Nulla, sit, amet...|
+|                  []|
+|[Cras, turpis, nu...|
+|                  []|
+|[Suspendisse, sed...|
++--------------------+
+"""
 
+# explode = dataframe.select(explode(split(dataframe.value, '\s+')).alias("Palavras")).show()
+"""
+Explode
++-----------+            
+|   Palavras|            
++-----------+            
+|      Lorem|            
+|      ipsum|            
+|      dolor|            
+|        sit|            
+|      amet,|            
+|consectetur|            
+| adipiscing|            
+|      elit.|            
+|         In|            
+|   iaculis,|            
+|     ligula|            
+|       quis|            
+|   lobortis|            
+|   aliquam,|            
+|         mi|            
+|       orci|            
+|   faucibus|            
+|     massa,|            
+|         eu|            
+|  imperdiet|            
++-----------+            
+only showing top 20 rows 
+"""
 
+count = dataframe.select(explode(split(dataframe.value, '([,.?!]?\s+[.,?!]?)+')).alias("Palavras")).groupBy("palavras").count().show()
+"""
+count
++------------+-----+     
+|    palavras|count|     
++------------+-----+     
+|      velit,|    1|     
+|       eros.|    2|     
+|consectetur.|    2|     
+|        odio|    3|     
+|         Sed|    3|     
+|    volutpat|    2|     
+|     pretium|    3|     
+|   volutpat,|    1|     
+|       nisl.|    1|     
+|     lectus,|    1|     
+|        vel.|    1|     
+|     Quisque|    2|     
+|   volutpat.|    1|     
+|    sagittis|    3|     
+|   hendrerit|    2|     
+|       netus|    1|     
+|       velit|    3|     
+|sollicitudin|    2|     
+|  hendrerit.|    1|     
+|       lorem|    2|     
++------------+-----+     
+only showing top 20 rows 
+"""
 
-
+# print(dataframe.value) # Column<b'value'>
 
 spark.stop()
